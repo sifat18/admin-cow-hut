a864c0abb8e4d4fd5aa502565c8ae069d392d5df
sifat18 <ibnsifat@gmail.com>
2023-07-02 04:20:25 +0600

    update order retreival done

--- src/modules/order/orderController.ts
+++ src/modules/order/orderController.ts
@@ -3,6 +3,9 @@ import catchAsync from "../../shared/catchAsync";
 import { IOrder } from "./orderInterface";
 import reponseFormat from "../../shared/responseFormat";
 import { createOrderService, getAllOrderService } from "./orderService";
+import mongoose from "mongoose";
+import APIError from "../../errorHelpers/APIError";
+import { Order } from "./orderModel";
 
 export const createOrder: RequestHandler = catchAsync(
   async (req: Request, res: Response) => {
@@ -17,7 +20,47 @@ export const createOrder: RequestHandler = catchAsync(
 );
 // get all
 export const getAllOrders = catchAsync(async (req: Request, res: Response) => {
-  const result = await getAllOrderService();
+  let result = await getAllOrderService();
+  let userIdAsObjecId = new mongoose.Types.ObjectId(req.user?._id);
+  // const exists = await Order.aggregate([
+  //   { $match: { buyer: userIdAsObjecId } },
+  // ]);
+
+  // // const exists = result?.filter((order) =>
+  // //   order.buyer?.equals(userIdAsObjecId)
+  // // );
+  if (req.user?.role === "admin") {
+    result = result;
+  } else if (req.user?.role === "buyer") {
+    const exists = await Order.aggregate([
+      { $match: { buyer: userIdAsObjecId } },
+      {
+        $lookup: {
+          from: "cows",
+          localField: "cow",
+          foreignField: "_id",
+          as: "cowData",
+        },
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "buyer",
+          foreignField: "_id",
+          as: "buyerData",
+        },
+      },
+    ]);
+
+    result = exists?.length > 0 ? exists : [];
+  } else if (req.user?.role === "seller") {
+    const orders = await Order.find({}).populate({
+      path: "cow",
+      match: { seller: userIdAsObjecId },
+    });
+
+    result = orders.filter((order) => order.cow !== null);
+  }
 
   reponseFormat<IOrder[]>(res, {
     statusCode: 200,
--- src/modules/order/orderRoute.ts
+++ src/modules/order/orderRoute.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { createOrder, getAllOrders } from "./orderController";
+import auth from "../../middlewears/auth";
+import { Admin_ROLE } from "../admin/adminConstant";
 
 const router = express.Router();
 
-router.post("/orders", createOrder);
-router.get("/orders", getAllOrders);
+router.post("/orders", auth("buyer"), createOrder);
+router.get("/orders", auth("seller", "buyer", Admin_ROLE.ADMIN), getAllOrders);
 
 export const orderRoutes = router;
